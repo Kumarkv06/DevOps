01ad9188745b9028f62bf3ac3900fa857ea9a58c
Kumarkv06 <kumarvenkatesh061103@gmail.com>
2025-06-05 09:08:25 +0530

    Hii

---
+++ src/main/java/devops/muktalingaraj/App.java
@@ -0,0 +1,20 @@
+package devops.muktalingaraj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public long calculateFactorial(int number) { 
+		if (number < 0) { 
+		throw new IllegalArgumentException("Number must be non-negative"); 
+		} 
+		long factorial = 1; 
+		for (int i = 1; i <= number; i++) { 
+		factorial *= i; 
+		} 
+		return factorial; 
+		} 
+
+}
